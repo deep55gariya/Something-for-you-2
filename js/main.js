5b8e6813cb29d6beb1cb504baff2eda63fccc22c
Deepak Gariya <97430393+deep55gariya@users.noreply.github.com>
2025-04-17 23:38:41 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U ').split('')
+    const titles = ('Chalo Madam ji WePlay Khelne 😁😘 ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
